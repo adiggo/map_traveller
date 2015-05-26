0bc05346d3ce31e5f764dc93e7f5973384b61af4
Shawn <adiggo@gmail.com>
2015-05-26 07:15:04 -0700

    Update User.sql

--- sqlsrc/User.sql
+++ sqlsrc/User.sql
@@ -1,4 +1,4 @@
 Create database IF NOT EXISTS Users;
 CREATE USER 'luss'@'localhost' IDENTIFIED BY 'luss';
-GRANT SELECT, INSERT, UPDATE, DELETE ON Users.* TO 'traveller@localhost' IDENTIFIED BY 'luss';
+GRANT SELECT, INSERT, UPDATE, DELETE ON Users.* TO 'luss@localhost' IDENTIFIED BY 'luss';
 
