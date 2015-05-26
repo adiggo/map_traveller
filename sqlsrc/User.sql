6d17726b740d05a2e917330edd398db1b925a3c5
xiaoping <xiaoping@talkme.im>
2015-05-26 16:51:49 -0700

    add user.sql

--- sqlsrc/User.sql
+++ sqlsrc/User.sql
@@ -2,3 +2,17 @@ Create database IF NOT EXISTS Users;
 CREATE USER 'luss'@'localhost' IDENTIFIED BY 'luss';
 GRANT SELECT, INSERT, UPDATE, DELETE ON Users.* TO 'luss'@'localhost' IDENTIFIED BY 'luss';
 
+CREATE TABLE IF NOT EXISTS Users.User(
+    id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    fid varchar(200),
+    wid varchar(200),
+    lname varchar(200),
+    fname varchar(200),
+    email varchar(20),
+    time_created datetime NOT NULL,
+    time_modified datetime,
+    PRIMARY KEY(id),
+    KEY(fid),
+    KEY(wid)
+)
+    
