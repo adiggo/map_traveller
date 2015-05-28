1f3948b044b98d6530e526a2c9816be67c2c863e
xiaoping <xiaoping@talkme.im>
2015-05-28 16:32:57 -0700

    merge

--- map-traveller.py
+++ map-traveller.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for('login'))
+@app.route('/login')
+def login():
+    
+    
 
-
+@app.route('/')
 if __name__ == '__main__':
     app.run()
