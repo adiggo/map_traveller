41713e91cc72fc858b6a0a3290693c33c36af6b5
adiggo <adiggo@gmail.com>
2015-05-29 16:40:58 -0700

    add more fields into user table. Use bcrypt to encode password

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,3 @@
-#imports
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
@@ -8,5 +7,6 @@ import os
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://luss:luss@localhost:3306/Users'
 db = SQLAlchemy(app)
--- api/model.py
+++ api/model.py
@@ -1,6 +1,6 @@
 from api import db
-
-
+from api import bcrypt
+from sqlalchemy import func
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -9,17 +9,36 @@ class user(db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), nullable=False)
-    email = db.Column(db.String(40))
-    password = db.Column(db.String(40))
-    fid = db.Column(db.String(40))
-    wid = db.Column=(db.String(40))
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(200))
+    fid = db.Column(db.String(200))
+    wid = db.Column(db.String(200))
+    dob = db.Column(db.DateTime)
+    # need to discuss whether store it in db or file system
+    profile_photo = db.Column(db.BLOB)
+    create_time = db.Column(db.DateTime, nullable = False, default=func.now())
+    update_time = db.Column(db.DateTime)
+
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
-        self.password = password
+        self.password = bcrypt.generate_password_hash(password)
 
+class place(db.Model):
 
+    __tablename__ = "place"
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable = False)
+    country = db.Column(db.String(100), nullable = False)
+    continent = db.Column(db.String(100), nullable = False)
+    def __init__(self, city, state, country, continent):
+        self.city = city
+        self.state = state
+        self.country = country
+        self.continent = continent
 
 
