318c33ede3205602a84889dcea0573a32a9e1366
adiggo <adiggo@gmail.com>
2015-06-03 21:50:03 -0700

    clean up

--- db_create.py
+++ db_create.py
@@ -1,8 +1,9 @@
 from api import db
 from api.model import User
+from datetime import datetime
 
 db.create_all()
 
-db.session.add(User("li", "adiggo@gmail.com", "pass"))
+db.session.add(User("li", "adiggo@gmail.com", "pass", 2, None, None, None,datetime.now(), datetime.now()))
 
 db.session.commit()
